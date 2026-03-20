a52820c64d3874343889471113c0163555b4ae1a
Lynn <haag.lynn1703@gmail.com>
2026-03-20 20:34:59 +0100

    funtions and set up for VR googles

--- Assets/Scripts/PhoneTap.cs
+++ Assets/Scripts/PhoneTap.cs
@@ -5,12 +5,24 @@ public class PhoneTap : MonoBehaviour
 {
     public BatteryManager batteryManager;
     public float tapAmount = 10f;
+    private bool triggerWasPressed = false;
 
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             Tap();
+        }
+
+        UnityEngine.XR.InputDevice rightHand = UnityEngine.XR.InputDevices.GetDeviceAtXRNode(UnityEngine.XR.XRNode.RightHand);
+        if (rightHand.isValid)
+        {
+            rightHand.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out bool triggerPressed);
+            if (triggerPressed && !triggerWasPressed)
+            {
+                Tap();
+            }
+            triggerWasPressed = triggerPressed;
         }
     }
 
